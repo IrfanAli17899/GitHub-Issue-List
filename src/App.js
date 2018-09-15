64de587e577c98d310c765210585b2471f986e3f
IrfanAli17899 <irfanali.17899@gmail.com>
2018-09-15 12:18:16 +0500

    boilerplate

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <h1 align="center">Hello World</h1>
+    );
+  }
+}
+
+export default App;
